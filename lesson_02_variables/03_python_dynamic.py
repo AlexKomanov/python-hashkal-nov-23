50b0986dffb1f22f9cf6e95ce95b1a5022ef0caf
AlexKomanov <akomanov88@gmail.com>
2023-11-07 19:07:12 +0200

    adding comments file

--- lesson_02_variables/03_python_dynamic.py
+++ lesson_02_variables/03_python_dynamic.py
@@ -9,3 +9,9 @@ print(type(my_name))
 my_name = False
 
 print(type(my_name))
+
+a = 30
+b = 20
+# del a
+print(b)
+print(a)
---
+++ lesson_02_variables/04_comments.py
@@ -0,0 +1,28 @@
+# print("1")
+# print("2")
+print("3")
+# print("4")
+print("5")
+
+age = 35+1  # New function to add 1
+age_1 = 35+1  # TODO: remove this function
+age_2 = 35+1  # Continue working on this
+
+name = "Alexander"
+print(name)
+
+comment_1 = """
+Alex
+Alex
+Alex
+"""
+
+comment_2 = '''
+Alexander
+Alexander
+Alexander
+'''
+
+print(comment_2)
+print(comment_1)
+print(type(comment_1))
