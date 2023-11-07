0284a7e9e0ef4959fb979c803910c82a1ce4ba95
AlexKomanov <akomanov88@gmail.com>
2023-11-07 18:13:53 +0200

    adding 01_variables.py

---
+++ lesson_02_variables/01_variables.py
@@ -0,0 +1,24 @@
+my_age = 66
+
+my_last_name = "Komanov"
+
+_name = "alex"
+
+# date is not Date
+
+date = "today"
+Date = "tomorrow"
+
+print(date)
+print(Date)
+print(id(date))
+print(id(Date))
+
+
+my_class = "my class"
+
+# MyLastName - Pascal Case
+# myLastName - Camel Case
+# my_last_name - Snake Case
+
+
