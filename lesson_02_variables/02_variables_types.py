86431142a9a7afc594b978df25f1c372b0242de7
AlexKomanov <akomanov88@gmail.com>
2023-11-07 18:44:41 +0200

    adding files

---
+++ lesson_02_variables/02_variables_types.py
@@ -0,0 +1,26 @@
+# byte short int long
+# float double
+
+age = 0
+height = -175.545453534534
+name = 'Alex Komanov'
+isTrue = False  # True
+
+my_list = ["age", "apple", 5, True, "age"]
+my_tuple = ("age", "apple", 5, True, "age")
+my_set = {"age", "apple", 5, True, "age"}
+my_dict = {"age": 35, "name": "Alex"}
+
+print(my_list)
+print(my_tuple)
+print(my_set)
+print(my_dict)
+print("---------------------")
+print(type(age))
+print(type(height))
+print(type(name))
+print(type(isTrue))
+print(type(my_list))
+print(type(my_tuple))
+print(type(my_set))
+print(type(my_dict))
---
+++ lesson_02_variables/03_python_dynamic.py
@@ -0,0 +1,11 @@
+my_name = "Alex"
+
+print(type(my_name))
+
+my_name = 15
+
+print(type(my_name))
+
+my_name = False
+
+print(type(my_name))
