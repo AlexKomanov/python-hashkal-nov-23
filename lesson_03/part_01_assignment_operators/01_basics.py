c0e0e54da900090f3004f5e95b8988d1c1cd1900
AlexKomanov <akomanov88@gmail.com>
2023-11-14 18:04:21 +0200

    adding assignment basics

---
+++ lesson_03/part_01_assignment_operators/01_basics.py
@@ -0,0 +1,27 @@
+my_current_age_as_integer = 35
+print(my_current_age_as_integer)
+
+my_current_age_as_integer += 1
+print(my_current_age_as_integer)
+
+my_current_age_as_integer -= 1
+print(my_current_age_as_integer)
+
+my_current_age_as_integer *= 2
+print(my_current_age_as_integer)
+
+my_current_age_as_integer /= 3
+print(my_current_age_as_integer)
+
+my_current_age_as_integer //= 3
+print(my_current_age_as_integer)
+
+my_current_age_as_integer %= 4
+print(my_current_age_as_integer)
+
+my_current_age_as_integer **= 4
+print(my_current_age_as_integer)
+
+
+
+
