f025b7327f6b036fa83e43addb083782ac823d6f
AlexKomanov <akomanov88@gmail.com>
2023-11-14 20:06:36 +0200

    adding class tasks

---
+++ lesson_03/class_tasks/02_solution.py
@@ -0,0 +1,8 @@
+number = int(input("Enter your number: "))
+print(number)
+
+number %= 10
+
+print(number)
+
+print(number == 2)
---
+++ lesson_03/class_tasks/03_solution.py
@@ -0,0 +1,6 @@
+number_1 = int(input("Enter your number: "))
+number_2 = int(input("Enter your number: "))
+
+print(number_1 % 7 == 0 and number_2 % 7 == 0)
+
+
