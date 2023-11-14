461fc230d67e249d6daa6cc4cb7c6022fecd1a91
AlexKomanov <akomanov88@gmail.com>
2023-11-14 19:09:08 +0200

    logical operators

---
+++ lesson_03/part_03_logical_operators/01_operator_and.py
@@ -0,0 +1,9 @@
+print((5 == 5) and (6 == 6))  # &&   &
+print((5 > 5) and (6 == 6))
+print((5 != 5) and (6 != 6))
+print((5 == 5) and (6 != 6))
+
+print((5 == 5) and (6 == 6) and (5 == 5) and (6 == 6))
+
+
+print((5 == 5) and (6 == 6) and (5 == 5) and (6 != 6))
---
+++ lesson_03/part_03_logical_operators/03_operator_not.py
@@ -0,0 +1,2 @@
+print(not (6 == 6))  # !
+print(not (6 != 6))
