d172eed7afafd99cba5fc40a3f4f3204c0c9101b
AlexKomanov <akomanov88@gmail.com>
2023-11-14 19:09:23 +0200

    comparison operators

---
+++ lesson_03/part_02_comparison_operators/01_basics.py
@@ -0,0 +1,20 @@
+print("5 == 5", 5 == 5)
+
+isTrue = 5 == 6
+print(type(isTrue))
+
+print("5 != 5", 5 != 5)
+print("5 != 6", 5 != 6)
+
+print("5 < 6", 5 < 6)
+print("6 < 6", 6 < 6)
+
+print("5 > 6", 5 > 6)
+print("5 > 3", 5 > 3)
+
+print("5 >= 5", 5 >= 5)
+print("5 >= 3", 5 >= 3)
+print("5 >= 6", 5 >= 6)
+
+print("alex == alex", "alex" == "alex")
+print("alex == Alex", "alex" == "Alex")
