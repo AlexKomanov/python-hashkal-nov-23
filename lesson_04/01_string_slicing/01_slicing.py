6ee6f2ea99cb85e4bf122fba546f7085201663dc
AlexKomanov <akomanov88@gmail.com>
2023-11-21 17:58:43 +0200

    string slicing

---
+++ lesson_04/01_string_slicing/01_slicing.py
@@ -0,0 +1,13 @@
+greeting = "Hello World!"
+
+print(greeting)
+print(greeting[0])
+print(greeting[0:4+1])
+print(greeting[6:])
+print(greeting[6:])
+print(greeting[-6:-2])
+
+print("=========== With Step ===============")
+print(greeting[0:12:1])
+print(greeting[0:12:6])
+print(greeting[::-1])
---
+++ lesson_04/01_string_slicing/02_string_immutable.py
@@ -0,0 +1,8 @@
+name = "Alexander"
+print(id(name))
+
+new_name = "A" + name[1:]
+
+print(name == new_name)
+
+print(id(new_name))
