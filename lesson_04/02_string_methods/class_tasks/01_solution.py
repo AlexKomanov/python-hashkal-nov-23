094ca6dcf02e268ffa192db8413c3316e316b2f0
AlexKomanov <akomanov88@gmail.com>
2023-11-21 19:28:16 +0200

    string methods class tasks

---
+++ lesson_04/02_string_methods/class_tasks/01_solution.py
@@ -0,0 +1,3 @@
+str1 = "efEdSdfEtet"
+print(str1)
+print(str1.lower().count("e"))
---
+++ lesson_04/02_string_methods/class_tasks/02_solution.py
@@ -0,0 +1,3 @@
+str1 = "efEdSfgfdgdgdfdfEtet"
+print(str1)
+print(str1[0:3].upper() + str1[3:-3].lower() + str1[-3:].upper())
