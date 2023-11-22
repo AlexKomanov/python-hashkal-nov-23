b8cbd4f48dba998f1d388672870a7e97e47c1266
AlexKomanov <akomanov88@gmail.com>
2023-11-22 05:41:50 +0200

    adding class work

---
+++ lesson_04/04_lists/01_lists_basics/02_list_index_slicing.py
@@ -0,0 +1,9 @@
+my_list = ["Alex", "Ron", "Dima", "Hello", "World", "Sun"]
+print(my_list[0])
+print(my_list[-1])
+print(my_list[::-1])
+print(my_list[2:5])
+print(my_list[2:])
+print(my_list)
+my_list[0] = "Alexander"
+print(my_list)
---
+++ lesson_04/04_lists/01_lists_basics/03_list_type_elements.py
@@ -0,0 +1,9 @@
+students_grades = [75, 80, 90, 85, 65, "Alex"]
+
+print(students_grades[0] + 10)
+print(students_grades[4] + 10)
+# print(students_grades[-1] + 10) # TypeError: can only concatenate str (not "int") to str
+
+print(len(students_grades))
+
+
